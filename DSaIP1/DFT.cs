def1708628117a36e848c502410eb4e3e3990012
A1exRuf <AlexeyRufeev@gmail.com>
2025-02-10 20:23:55 +0300

    Add project files.

---
+++ DSaIP1/DFT.cs
@@ -0,0 +1,40 @@
+﻿using System.Numerics;
+
+namespace DSaIP1;
+
+internal class DFT
+{
+    public static Complex[] ProcessDFT(Complex[] input)
+    {
+        int N = input.Length;
+        Complex[] output = new Complex[N];
+        for (int k = 0; k < N; k++)
+        {
+            Complex sum = Complex.Zero;
+            for (int n = 0; n < N; n++)
+            {
+                double angle = -2 * Math.PI * k * n / N;
+                sum += input[n] * Complex.Exp(new Complex(0, angle));
+            }
+            output[k] = new Complex(Math.Round(sum.Real, 2), Math.Round(sum.Imaginary, 2));
+        }
+        return output;
+    }
+
+    public static double[] ProcessIDFT(Complex[] input)
+    {
+        int N = input.Length;
+        double[] output = new double[N];
+        for (int n = 0; n < N; n++)
+        {
+            Complex sum = Complex.Zero;
+            for (int k = 0; k < N; k++)
+            {
+                double angle = 2 * Math.PI * k * n / N;
+                sum += input[k] * Complex.Exp(new Complex(0, angle));
+            }
+            output[n] = Math.Round(sum.Real / N, 2);
+        }
+        return output;
+    }
+}
---
+++ DSaIP1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+
+namespace DSaIP1;
+
+class Program
+{
+    static void Main()
+    {
+        Complex[] data = new Complex[]
+        {
+            1, -1, 4, 2, -2, 3, 5, -3,
+            6, -4, 7, -5, 8, -6, 9, -7,
+            10, -8, 11, -9, 12, -10, 13, -11,
+            14, -12, 15, -13, 16, -14, 17, -15
+        };
+
+        Complex[] spectrum = DFT.ProcessDFT(data);
+        Console.WriteLine("ДПФ:");
+        for (int k = 0; k < spectrum.Length; k++)
+        {
+            Console.WriteLine($"S[{k}] = {spectrum[k]}");
+        }
+
+        double[] restored = DFT.ProcessIDFT(spectrum);
+        Console.WriteLine("\nОДПФ:");
+        for (int n = 0; n < restored.Length; n++)
+        {
+            Console.WriteLine($"S[{n}] = {restored[n]}");
+        }
+    }
+}
