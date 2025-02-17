6c12f39eba5d4843a9e411c78b8ae1d4c4bf165b
A1exRuf <AlexeyRufeev@gmail.com>
2025-02-17 22:06:32 +0300

    Amplitude and Phase

--- DSaIP1/DFT.cs
+++ DSaIP1/DFT.cs
@@ -1,4 +1,5 @@
-﻿using System.Numerics;
+﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace DSaIP1;
 
@@ -34,6 +35,30 @@ internal class DFT
                 sum += input[k] * Complex.Exp(new Complex(0, angle));
             }
             output[n] = Math.Round(sum.Real / N, 2);
+        }
+        return output;
+    }
+
+    public static Complex[] GetAmplitude(Complex[] input)
+    {
+        int N = input.Length;
+        Complex[] output = new Complex[N];
+        for (int n = 0; n < N; n++)
+        {
+            double real = Math.Abs(input[n].Real);
+            double imaginary = Math.Abs(input[n].Imaginary);
+            output[n] = new Complex(real, imaginary);
+        }
+        return output;
+    }
+
+    public static List<double> GetPhase(Complex[] input)
+    {
+        int N = input.Length;
+        List<double> output = [];
+        for (int n = 0; n < N; n++)
+        {
+            output.Add(Math.Round(Math.Atan(input[n].Imaginary / input[n].Real), 2));
         }
         return output;
     }
--- DSaIP1/Program.cs
+++ DSaIP1/Program.cs
@@ -6,20 +6,40 @@ class Program
 {
     static void Main()
     {
+        //Complex[] data = new Complex[]
+        //{
+        //    1, -1, 4, 2, -2, 3, 5, -3,
+        //    6, -4, 7, -5, 8, -6, 9, -7,
+        //    10, -8, 11, -9, 12, -10, 13, -11,
+        //    14, -12, 15, -13, 16, -14, 17, -15
+        //};
+
         Complex[] data = new Complex[]
         {
-            1, -1, 4, 2, -2, 3, 5, -3,
-            6, -4, 7, -5, 8, -6, 9, -7,
-            10, -8, 11, -9, 12, -10, 13, -11,
-            14, -12, 15, -13, 16, -14, 17, -15
+            1, -1, 4, 2, -2, 3
         };
 
         Complex[] spectrum = DFT.ProcessDFT(data);
         Console.WriteLine("ДПФ:");
-        for (int k = 0; k < spectrum.Length; k++)
+        for (int n = 0; n < spectrum.Length; n++)
         {
-            Console.WriteLine($"S[{k}] = {spectrum[k]}");
+            Console.WriteLine($"S[{n}] = {spectrum[n]}");
         }
+
+        Complex[] amplitude = DFT.GetAmplitude(spectrum);
+        Console.WriteLine("\nАмплитуда:");
+        for (int n = 0; n < amplitude.Length; n++)
+        {
+            Console.WriteLine($"S[{n}] = {amplitude[n]}");
+        }
+
+        List<double> phase = DFT.GetPhase(spectrum);
+        Console.WriteLine("\nФаза:");
+        for (int n = 0; n < amplitude.Length; n++)
+        {
+            Console.WriteLine($"S[{n}] = {phase[n]}");
+        }
+
 
         double[] restored = DFT.ProcessIDFT(spectrum);
         Console.WriteLine("\nОДПФ:");
